a4e7a9faaf1b9cc640fc1c12ac04d07ce403f1b8
abc <wingo.he@qq.com>
2020-11-18 10:35:34 +0800

    fix

--- app/src/main/java/com/example/cameracompare/MainActivity.java
+++ app/src/main/java/com/example/cameracompare/MainActivity.java
@@ -14,9 +14,14 @@ import android.util.Log;
 import android.util.Size;
 import android.view.TextureView;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,7 +33,6 @@ import com.example.cameracompare.camera.FormatConvert;
 import com.example.cameracompare.camera.GLPanel;
 
 import java.nio.ByteBuffer;
-import java.text.Normalizer;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getCanonicalName();
@@ -42,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup mGroup;
     private Camera1Helper camera1;
     private Camera2Helper camera2;
+    private Spinner mRotateAngle;
+    private Spinner mCameraList;
+    private CheckBox mFlip;
     private int cameraId = 0;
     private int rotateAngle = 0;
     private boolean flip = true;
@@ -73,6 +80,34 @@ public class MainActivity extends AppCompatActivity {
         mPreview2.setVisibility(mChecker.isChecked() ? View.GONE : View.VISIBLE);
         mRgba =(RadioButton)findViewById(R.id.rgba);
         mGroup = (RadioGroup)findViewById(R.id.camera);
+        mFlip = (CheckBox)findViewById(R.id.flip);
+        flip = mFlip.isChecked();
+        mFlip.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                flip = b;
+            }
+        });
+        mRotateAngle = (Spinner)findViewById(R.id.rotateAngle);
+        mCameraList = (Spinner)findViewById(R.id.cameraList);
+        Integer[] angle = new Integer[4];
+        angle[0] = 0;
+        angle[1] = 90;
+        angle[2] = 180;
+        angle[3] = 270;
+        mRotateAngle.setAdapter(new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1, angle));
+        mRotateAngle.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> var1, View var2, int var3, long var4){
+                rotateAngle = (int)mRotateAngle.getSelectedItem();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        mCameraList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Camera2Helper.cameraList(this)));
         mGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int id) {
@@ -92,8 +127,11 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this.getApplicationContext(), "No Camera to start.", Toast.LENGTH_LONG);
                     return;
                 }
+                String vid = (String) mCameraList.getSelectedItem();
+                cameraId = Integer.parseInt(vid);
                 if(mChecker.isChecked()) {
                     camera1 = new Camera1Helper.Builder()
+                            .previewSize(new Point(640, 480))
                             .previewViewSize(new Point(640,480))
                             .rotation(rotateAngle)
                             .specificCameraId(cameraId)
@@ -108,7 +146,7 @@ public class MainActivity extends AppCompatActivity {
                             .rgb32Format(mRgba.isChecked())
                             .specificCameraId(String.valueOf(cameraId))
                             .context(MainActivity.this.getBaseContext())
-                            .maxPreviewSize(new Size(800, 600))
+                            .maxPreviewSize(new Size(640, 480))
                             .previewSize(new Size(640, 480))
                             .cameraListener(listener2)
                             .build();
@@ -145,7 +183,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPreview(byte[] data, Camera camera) {
                 Camera.Size sz = camera.getParameters().getPreviewSize();
-                Log.d(TAG, String.format("camera1 width:%d - height:%d", sz.width, sz.height));
+                Log.d(TAG, String.format("camera1.Id:%d - rotate:%d - flip:%d - width:%d - height:%d", cameraId, rotateAngle, flip ? 1 : 0, sz.width, sz.height));
                 Bitmap bmp = mConvert1.nv21ToBitmap(data, sz.width, sz.height);
                 Bitmap front = mConvert1.rotateBitmap(bmp, rotateAngle, flip);
                 ByteBuffer buf = mConvert1.bitmapBuffer(front);
@@ -185,7 +223,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onPreview(byte[] data, int width, int height, boolean isRgb32, CameraDevice camera) {
-                Log.d(TAG, String.format("camera2 width:%d - height:%d", width, height));
+                Log.d(TAG, String.format("camera2.Id:%d - rotate:%d - flip:%d - width:%d - height:%d", cameraId, rotateAngle, flip ? 1 : 0, width, height));
                 Bitmap bmp = isRgb32 ? mConvert2.rgbaToBitmap(data, width, height) : mConvert2.nv21ToBitmap(data, width, height);
                 Bitmap front = mConvert2.rotateBitmap(bmp, rotateAngle, flip);
                 ByteBuffer buf = mConvert2.bitmapBuffer(front);
@@ -211,7 +249,7 @@ public class MainActivity extends AppCompatActivity {
             public void onTick(long l) {
                 long qps = mFrameCount - frameLast;
                 frameLast = mFrameCount;
-                String msg = String.format("CameraInfo: QPS:%d - width:%d - height:%d", qps, mPreviewWidth, mPreviewHeight);
+                String msg = String.format("camera2.Id:%d - QPS:%d - rotate:%d - flip:%d - width:%d - height:%d", cameraId, qps, rotateAngle, flip ? 1 : 0, mPreviewWidth, mPreviewHeight);
                 Log.d(TAG, msg);
                 mInfo.setText(msg);
             }
